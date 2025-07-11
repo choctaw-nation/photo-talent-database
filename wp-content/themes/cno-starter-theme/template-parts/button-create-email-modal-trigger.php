3bca624aca5eb84ed05f00f4ec43f8657a122bbf
KJ Roelke <kroelke@choctawnation.com>
2025-07-11 10:39:09 -0500

    init modal

---
+++ wp-content/themes/cno-starter-theme/template-parts/button-create-email-modal-trigger.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Create Email Modal Trigger Button
+ *
+ * @package ChoctawNation
+ */
+
+?>
+<button type="button" class="btn btn-outline-primary position-fixed end-0 bottom-0 m-3 z-3 shadow-lg" data-bs-toggle="modal" data-bs-target="#create-email-modal">
+	Create Email
+</button>
---
+++ wp-content/themes/cno-starter-theme/template-parts/modal-create-email.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Create Email Modal Template
+ *
+ * @package ChoctawNation
+ */
+
+?>
+<div class="modal" tabindex="-1" id="create-email-modal">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5">Create Email</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				<p>Modal body text goes here.</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Clear Selection</button>
+				<button type="button" class="btn btn-primary">Send Email</button>
+			</div>
+		</div>
+	</div>
+</div>
