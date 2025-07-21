7bef79cb59f032aaf65f8cd6ff0780cfc89ac505
KJ Roelke <kroelke@choctawnation.com>
2025-07-21 13:40:19 -0500

    add carousel controls

--- wp-content/themes/cno-starter-theme/template-parts/talent-preview/card-carousel-top.php
+++ wp-content/themes/cno-starter-theme/template-parts/talent-preview/card-carousel-top.php
@@ -10,7 +10,7 @@ $slug        = 'carousel-' . sanitize_title( get_the_title( $talent_id ) );
 $image_names = array( 'front', 'back', 'left', 'right', 'three_quarters' );
 $image_args  = array(
 	'loading' => 'lazy',
-	'class'   => 'h-100 w-100 object-fit-cover d-block carousel-item__image',
+	'class'   => 'h-100 w-100 object-fit-cover d-block carousel-item__image z-1 position-relative',
 );
 
 $images = array();
@@ -25,7 +25,15 @@ foreach ( $image_names as $name ) {
 ?>
 <div class="card-img-top overflow-hidden">
 	<div id="<?php echo esc_attr( $slug ); ?>" class="carousel slide">
-		<div class="carousel-indicators mb-0">
+		<?php $controls = array( 'prev', 'next' ); ?>
+		<?php foreach ( $controls as $control ) : ?>
+		<button class="carousel-control-<?php echo esc_attr( $control ); ?> z-3" type="button" data-bs-target="#<?php echo esc_attr( $slug ); ?>"
+				data-bs-slide="<?php echo esc_attr( $control ); ?>">
+			<span class="carousel-control-<?php echo esc_attr( $control ); ?>-icon" aria-hidden="true"></span>
+			<span class="visually-hidden"><?php echo ucfirst( $control ); ?></span>
+		</button>
+		<?php endforeach; ?>
+		<div class="carousel-indicators z-3 mb-0">
 			<?php foreach ( $images as $name => $image ) : ?>
 			<?php $active_class = 'front' === $name ? 'active' : ''; ?>
 			<button type="button" data-bs-target="#<?php echo esc_attr( $slug ); ?>" data-bs-slide-to="<?php echo esc_attr( array_search( $name, array_keys( $images ), true ) ); ?>"
@@ -33,12 +41,12 @@ foreach ( $image_names as $name ) {
 					aria-label="<?php echo esc_attr( ucfirst( $name ) ); ?>"></button>
 			<?php endforeach; ?>
 		</div>
+		<div class="carousel-item__overlay position-absolute z-2"></div>
 		<div class="carousel-inner">
 			<?php foreach ( $images as $name => $image ) : ?>
 			<div class="carousel-item <?php echo 'front' === $name ? 'active' : ''; ?>">
-				<div class="carousel-item__overlay position-absolute"></div>
 				<?php echo $image; ?>
-				<div class="carousel-caption p-0">
+				<div class="carousel-caption p-0 z-3">
 					<p class="mb-0"><?php echo 'three_quarters' === $name ? 'Three Quarters' : ucfirst( $name ); ?></p>
 				</div>
 			</div>
