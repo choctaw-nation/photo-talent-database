52b4a0e7a6debcbf4539b984bb2f7ddc42d3165f
KJ Roelke <kroelke@choctawnation.com>
2025-07-22 10:42:25 -0500

    fix sidebar filters

--- wp-content/themes/cno-starter-theme/src/js/talent/sidebarToggleHandler.ts
+++ wp-content/themes/cno-starter-theme/src/js/talent/sidebarToggleHandler.ts
@@ -1,20 +1,11 @@
+import Collapse from 'bootstrap/js/dist/collapse';
 import { BS_BREAKPOINTS } from '../utils/consts';
 
 export default function sidebarToggleHandler() {
 	const viewportWidth = window.innerWidth;
-	const sidebar = document.getElementById(
-		'sidebar-filters'
-	) as HTMLDetailsElement;
-	if ( viewportWidth > BS_BREAKPOINTS.lg ) {
-		sidebar.setAttribute( 'open', 'true' );
+	const sidebar = document.getElementById( 'sidebar-filters' ) as HTMLElement;
+	if ( viewportWidth < BS_BREAKPOINTS.lg ) {
+		const collapse = Collapse.getOrCreateInstance( sidebar );
+		collapse.hide();
 	}
-
-	window.addEventListener( 'resize', ( ev ) => {
-		const target = ev.target as Window;
-		if ( target.innerWidth > BS_BREAKPOINTS.lg ) {
-			sidebar.setAttribute( 'open', 'true' );
-		} else {
-			sidebar.removeAttribute( 'open' );
-		}
-	} );
 }
