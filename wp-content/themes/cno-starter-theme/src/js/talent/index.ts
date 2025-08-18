f85f5f5e2f5b95e6ef7125c4aa45b94f9901d44f
KJ Roelke <kroelke@choctawnation.com>
2025-08-18 16:38:05 -0500

    load js

--- wp-content/themes/cno-starter-theme/src/js/talent/index.ts
+++ wp-content/themes/cno-starter-theme/src/js/talent/index.ts
@@ -1,5 +1,7 @@
 import sidebarToggleHandler from './sidebarToggleHandler';
 import 'bootstrap/js/dist/dropdown';
+import './talent-preview-handler/controller';
+
 window.addEventListener( 'DOMContentLoaded', () => {
 	sidebarToggleHandler();
 } );
