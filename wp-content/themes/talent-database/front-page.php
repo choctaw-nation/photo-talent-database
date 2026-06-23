34d2bcd1c66221a17f4cd1e633cac8a709295bac
KJ Roelke <kroelke@choctawnation.com>
2026-06-23 13:28:02 -0500

    update header

--- wp-content/themes/talent-database/front-page.php
+++ wp-content/themes/talent-database/front-page.php
@@ -25,14 +25,17 @@ if ( is_user_logged_in() ) {
 	<?php wp_body_open(); ?>
 	<header>
 		<div class="container d-flex flex-column flex-md-row justify-content-between align-items-center py-3">
-			<a href="<?php echo is_user_logged_in() ? esc_url( home_url( '/talent' ) ) : esc_url( home_url( '/' ) ); ?>" class="d-flex align-items-center mb-3 mb-md-0 text-dark text-decoration-none">
-				<img src="<?php echo esc_url( get_template_directory_uri() . '/img/the-great-seal-min.svg' ); ?>" alt="Choctaw Nation of Oklahoma" width="200" height="50" />
+			<a href="<?php echo is_user_logged_in() ? esc_url( home_url( '/talent' ) ) : esc_url( home_url( '/' ) ); ?>" class="d-flex align-items-center mb-3 mb-md-0 gap-2 text-dark text-decoration-none">
+				<img src="<?php echo esc_url( get_template_directory_uri() . '/img/the-great-seal-min.svg' ); ?>" alt="Choctaw Nation of Oklahoma" height="150" loading="eager" />
 				<span><?php echo bloginfo( 'site_title' ); ?></span>
 			</a>
 		</div>
 	</header>
 	<main <?php post_class( 'container d-flex flex-column justify-content-center align-items-center flex-grow-1' ); ?>>
-		<h1 class="text-center mb-0"><div class="display-5">Choctaw Nation of Oklahoma</div><div class="display-1 fw-bold">Talent Database</div></h1>
+		<h1 class="text-center mb-0">
+			<div class="display-6">Choctaw Nation of Oklahoma</div>
+			<div class="display-2 fw-bold">Talent Database</div>
+		</h1>
 		<div class="d-flex flex-wrap column-gap-5 row-gap-3 w-100 justify-content-center">
 			<?php
 			$btn_classes = array(
@@ -52,7 +55,6 @@ if ( is_user_logged_in() ) {
 	<footer>
 		<div class="container d-flex flex-column flex-md-row justify-content-between align-items-center py-3">
 			<p class="mb-0">&copy; <?php echo gmdate( 'Y' ); ?> Choctaw Nation of Oklahoma</p>
-			<a href="<?php echo esc_url( home_url( '/privacy-policy' ) ); ?>" class="text-decoration-none">Privacy Policy</a>
 		</div>
 	</footer>
 	<?php wp_footer(); ?>
