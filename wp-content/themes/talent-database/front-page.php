5708de39a656a0136d70d5b159eb0f903a1b6b5c
KJ Roelke <kroelke@choctawnation.com>
2026-06-25 13:59:49 -0500

    fix a11y bug

--- wp-content/themes/talent-database/front-page.php
+++ wp-content/themes/talent-database/front-page.php
@@ -12,9 +12,8 @@ if ( is_user_logged_in() ) {
 get_header();
 ?>
 <main <?php post_class( 'container col-lg-8 text-center d-flex flex-column justify-content-center align-items-center flex-grow-1' ); ?>>
-	<h1 class="mb-0">
 		<div class="display-6">Choctaw Nation of Oklahoma</div>
-		<div class="display-2 fw-bold">Talent Database</div>
+	<h1 class="display-2 fw-bold mb-0">Talent Database
 	</h1>
 	<a href="/apply" class="btn btn-lg btn-black text-uppercase rounded-pill mb-3">Apply</a>
 	<p class="mb-0">This website is owned, operated, and authorized by the Choctaw Nation of Oklahoma. Information presented on this site is provided on behalf of the Choctaw Nation of Oklahoma.</p>
